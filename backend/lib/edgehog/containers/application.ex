928dc8d7f916711be98e3aa5a202cde4c969190f
Riccardo Binetti <riccardo.binetti@secomind.com>
2024-10-15 16:50:39 +0200

    feat(containers): add Containers.Application resource
    Allow representing an application, which is a set of containers implementing a
    specific functionality

---
+++ backend/lib/edgehog/containers/application.ex
@@ -0,0 +1,60 @@
+#
+# This file is part of Edgehog.
+#
+# Copyright 2024 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+defmodule Edgehog.Containers.Application do
+  @moduledoc false
+
+  use Edgehog.MultitenantResource,
+    domain: Edgehog.Containers,
+    extensions: [AshGraphql.Resource]
+
+  resource do
+    description """
+    An Application is a set of containers providing a specific functionality.
+    """
+  end
+
+  graphql do
+    type :application
+  end
+
+  actions do
+    defaults [:read]
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :name, :string do
+      allow_nil? false
+      public? true
+    end
+
+    attribute :description, :string do
+      public? true
+    end
+
+    timestamps()
+  end
+
+  postgres do
+    table "container_applications"
+  end
+end
